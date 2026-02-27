c066e8298e54ba1d0cce45fb366d83d05d202d4f
sourasish-mukherjee1904 <sunandmoon200519@gmail.com>
2026-02-27 18:36:20 +0530

    unscalled training set created

--- src/processing.py
+++ src/processing.py
@@ -23,12 +23,12 @@ scaler = StandardScaler()
 
 num_cols = ['ApplicantIncome','CoapplicantIncome','LoanAmount','Loan_Amount_Term']
 
-df[num_cols] = scaler.fit_transform(df[num_cols])
+# df[num_cols] = scaler.fit_transform(df[num_cols])
 X = df.drop('Loan_Status', axis=1)
 y = df['Loan_Status']
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
-X_train.to_csv("../data/X_train.csv",index=False)
+X.to_csv("../data/X_train.csv",index=False)
 X_test.to_csv("../data/X_test.csv",index=False)
-y_train.to_csv("../data/y_train.csv",index=False)
+y.to_csv("../data/y_train.csv",index=False)
 y_test.to_csv("../data/y_test.csv",index=False)
